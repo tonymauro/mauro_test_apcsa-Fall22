6d02f7c5bd5806992255f0d26a1e2be3290d764b
tonymauro <tony.mauro@sduhsd.net>
2022-08-17 12:56:42 -0700

    Add Unit-01

---
+++ Unit-01/src/StarsRunner.java
@@ -0,0 +1,20 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      
+      //call the methods needed to make the patterns on the word document
+      
+   }
+}
